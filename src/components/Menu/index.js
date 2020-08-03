226a9ffb07cebdd0a2e5817a717fb416031c510d
JG <diasusb@hotmail.com>
2020-08-02 23:20:49 -0300

    Corrigindo botão no mobile

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -7,23 +7,10 @@ import Button from '../Button';
 function Menu() {
   return (
     <nav className="Menu">
-      {/* <Link to="/">
-        <img className="Logo" src={Logo} alt="PythonFlix Logo" />
-      </Link>
-      {Route !== '/cadastro/video' && (
-        <Button as={Link} to="/cadastro/video">
-          Novo vídeo
-        </Button>
-      )}
-      {Route === '/cadastro/video' && (
-        <Button as={Link} to="/cadastro/categoria">
-          Novo Categoria
-        </Button>
-      )} */}
       <Link to="/">
         <img className="Logo" src={Logo} alt="PythonFlix Logo" />
       </Link>
-      <Button as={Link} to="/cadastro/video">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo vídeo
       </Button>
     </nav>
