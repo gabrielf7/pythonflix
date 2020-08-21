bacf59426e6d22f9e042b695ce67db43af669cf7
JG <diasusb@hotmail.com>
2020-08-21 19:37:39 -0300

    update

--- src/pages/Page404/index.js
+++ src/pages/Page404/index.js
@@ -7,12 +7,14 @@ import StylesPage404 from './styles';
 export const Page404 = () => (
   <PageDefault>
     <StylesPage404 align="center">
-        <h1>Error 404</h1>
-        <h4>Esta página não existe ou ainda está em desenvolvimento.</h4>
-        <Button as={Link} to="/">Volte para home</Button>
-        <p>Ou</p>
-        <h3>Acesse a Live do <strong>MarcoBruno na Twitch</strong></h3>
-        <Button as="a" href="https://www.twitch.tv/marcobrunodev">Acesse aqui</Button>
+      <h1>Error 404</h1>
+      <h4>Esta página não existe ou ainda está em desenvolvimento.</h4>
+      <Button as={Link} to="/">Volte para home</Button>
+    </StylesPage404>
+    <StylesPage404 align="center">
+      <p>Ou</p>
+      <h3>Acesse a Live do <strong>MarcoBruno na Twitch</strong></h3>
+      <Button as="a" href="https://www.twitch.tv/marcobrunodev">Acesse aqui</Button>
     </StylesPage404>
     {/* <Iframe src="https://mariosouto.com/flappy-bird-devsoutinho/" scrolling="no" frameborder="no" /> */}
   </PageDefault>
--- src/pages/Page404/styles.js
+++ src/pages/Page404/styles.js
@@ -1,15 +1,10 @@
 import styled from 'styled-components';
 
 export const StylesPage404 = styled.div`
-    /* width: 100%;
-    height: 482px;
     margin-top: 16px;
     margin-bottom: 16px;
-    border: none;
-    text-align: center; */
 
-    h3 strong {
-        border: 0;
+    h3 {
         border-bottom: 2px solid var(--pypinkblack);
     }
 `;
