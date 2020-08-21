62afc1916f7248f75d81b6d768384f5937731bc6
JG <diasusb@hotmail.com>
2020-08-21 19:00:58 -0300

    Alterando posição

--- src/pages/Page404/index.js
+++ src/pages/Page404/index.js
@@ -11,11 +11,11 @@ export const Page404 = () => (
       <h1>Error 404</h1>
       <h4>Esta página não existe ou ainda está em desenvolvimento.</h4>
       <Button as={Link} to="/">Volte para home</Button>
+      <p>Ou</p>
+      <h3>Acesse a Live do <strong>MarcoBruno na Twitch</strong></h3>
+      <Button as="a" href="https://www.twitch.tv/marcobrunodev">Acesse aqui</Button>
     </div>
     {/* <Iframe src="https://mariosouto.com/flappy-bird-devsoutinho/" scrolling="no" frameborder="no" /> */}
-    <p>Ou</p>
-    <h3>Acesse a Live do <strong>MarcoBruno na Twitch</strong></h3>
-    <Button as="a" href="https://www.twitch.tv/marcobrunodev">Acesse aqui</Button>
 
   </PageDefault>
 );
